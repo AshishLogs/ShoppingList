7a0d0ae98afed7a433e40770c8bef6b6b6758dbe
ashish <ashish_melp@sysmind.com>
2020-07-03 02:59:46 +0530

    initial commit - Starting the project

---
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by MelpApp-Ashish on 7/3/20.
+//  Copyright © 2020 iSingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
